ec28750ec3e77d3bbe20c02e1f72d04d6bb36b15
ELRJOR <carlitos_04jorge@hotmail.com>
2024-06-25 20:31:51 -0600

    hola

--- maquetadoRuth/output/script.js
+++ maquetadoRuth/output/script.js
@@ -24,3 +24,19 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
+
+document.getElementById('logoutButton').addEventListener('click', function() {
+  if (confirm('¿Estás seguro de que quieres cerrar la sesión?')) {
+      // Aquí puedes redirigir al usuario a la página de inicio de sesión o hacer otras acciones de cierre de sesión
+      // Por ejemplo, redirigir a la página de inicio
+      window.location.href = 'index.html'; // Cambia 'logout.php' por tu ruta de cierre de sesión real
+  }
+});
+
+document.getElementById('messagesLink').addEventListener('click', function (event) {
+  // Prompt user with a confirmation message
+  var confirmationMessage = 'Estás a punto de dirigirte a mensajeria, los cambios no guardados se perderán, ¿Deseas continuar?';
+  if (!confirm(confirmationMessage)) {
+      event.preventDefault(); // Cancel the navigation if the user cancels
+  }
+});

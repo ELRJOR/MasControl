4af0aece89d599f4b935028f48222a418af5acaa
ELRJOR <carlitos_04jorge@hotmail.com>
2024-06-26 17:34:35 -0600

    s

--- maquetadoRuth/output/script.js
+++ maquetadoRuth/output/script.js
@@ -25,38 +25,6 @@ if (toggleButton) {
 }
 });
 
-//parte del formulario de tutores
-document.getElementById("tutorForm").addEventListener("submit", async function(event) {
-    event.preventDefault();
-
-    const formData = {
-        name: document.getElementById("name").value,
-        lastname: document.getElementById("lastname").value,
-        address: document.getElementById("address").value,
-        phone: document.getElementById("phone").value,
-        email: document.getElementById("email").value
-    };
-
-    try {
-        const response = await fetch('http://localhost:3000/alta-tutor', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        });
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        console.log(data); // Puedes manejar la respuesta del servidor aquí
-
-    } catch (error) {
-        console.error('Error:', error);
-    }
-});
 
 
 document.getElementById('logoutButton').addEventListener('click', function() {

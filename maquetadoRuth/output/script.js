73c7c4853a421a317911a77fb7c2b24fa8028ec9
ELRJOR <carlitos_04jorge@hotmail.com>
2024-06-26 18:30:13 -0600

    x

--- maquetadoRuth/output/script.js
+++ maquetadoRuth/output/script.js
@@ -43,3 +43,44 @@ if (!confirm(confirmationMessage)) {
 }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const fechaPublicacionInput = document.getElementById('fecha_publicacion');
+    const fechaCierreInput = document.getElementById('fecha_cierre');
+
+    // Obtenemos la fecha actual en formato ISO (yyyy-mm-dd)
+    const fechaActual = new Date().toISOString().split('T')[0];
+
+    // Establecemos el atributo min de fecha de publicación como la fecha actual
+    fechaPublicacionInput.setAttribute('min', fechaActual);
+
+    // Agregamos un event listener para verificar la fecha de cierre al cambiar la fecha de publicación
+    fechaPublicacionInput.addEventListener('change', function() {
+        const fechaPublicacion = new Date(fechaPublicacionInput.value);
+        const fechaCierreMin = fechaPublicacionInput.value; // Obtener el valor de fecha de publicación como mínimo para fecha de cierre
+        const fechaCierreSeleccionada = fechaCierreInput.value; // Obtener el valor de fecha de cierre seleccionada
+
+        // Establecer el atributo min de fecha de cierre como mínimo para fecha de publicación
+        fechaCierreInput.setAttribute('min', fechaCierreMin);
+
+        // Verificar si la fecha de cierre seleccionada es inválida
+        if (fechaCierreSeleccionada < fechaCierreMin) {
+            fechaCierreInput.value = fechaCierreMin; // Restaurar la fecha mínima si la fecha seleccionada es inválida
+        }
+    });
+
+    // Agregamos un event listener para verificar la fecha de cierre al cambiar directamente la fecha de cierre
+    fechaCierreInput.addEventListener('change', function() {
+        const fechaCierre = new Date(fechaCierreInput.value);
+        const fechaPublicacion = new Date(fechaPublicacionInput.value);
+
+        // Verificar que la fecha de cierre no sea anterior a la fecha de publicación
+        if (fechaCierre < fechaPublicacion) {
+            fechaCierreInput.setCustomValidity('La fecha de cierre debe ser igual o posterior a la fecha de publicación');
+        } else {
+            fechaCierreInput.setCustomValidity('');
+        }
+    });
+});
+
+
+

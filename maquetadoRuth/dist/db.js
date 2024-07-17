acd377829efe5c6799e220e70b9a8305dac5fb1e
Jess4lxy <alex.mtz.rv17@gmail.com>
2024-07-17 17:14:37 -0600

    ya

--- maquetadoRuth/dist/db.js
+++ maquetadoRuth/dist/db.js
@@ -52,28 +52,28 @@ exports.buscarTramitePorId = buscarTramitePorId;
 exports.actualizarTramite = actualizarTramite;
 exports.eliminarTramite = eliminarTramite;
 const mssql = __importStar(require("mssql"));
-// Configuración para la conexión a la base de datos
-const dbConfig = {
-    user: 'Jessie',
-    password: '1234',
-    server: 'FAVORITECHILD',
-    database: 'MasControlDB',
-    options: {
-        encrypt: true,
-        trustServerCertificate: true,
-    },
-};
 // // Configuración para la conexión a la base de datos
 // const dbConfig: mssql.config = {
-//     user: 'AyalaPro',
+//     user: 'Jessie',
 //     password: '1234',
-//     server: '(Pon el nombre de tu servidor/computadora',
+//     server: 'FAVORITECHILD',
 //     database: 'MasControlDB',
 //     options: {
 //         encrypt: true,
 //         trustServerCertificate: true,
 //     },
 // };
+// Configuración para la conexión a la base de datos
+const dbConfig = {
+    user: 'AyalaPro',
+    password: '1234',
+    server: 'JORGEA',
+    database: 'MasControlDB',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true,
+    },
+};
 // Función para conectar a la base de datos
 function conectarBD() {
     return __awaiter(this, void 0, void 0, function* () {

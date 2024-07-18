08222ac53b05ba6db90943b14db6836317e10b8d
ELRJOR <carlitos_04jorge@hotmail.com>
2024-07-17 23:40:21 -0600

    x

--- maquetadoRuth/output/avisosTut.js
+++ maquetadoRuth/output/avisosTut.js
@@ -8,12 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
             data.forEach(aviso => {
                 const avisoCard = document.createElement('div');
-                avisoCard.classList.add('block', 'p-6', 'bg-white', 'rounded-lg', 'shadow-lg', 'text-center', 'transition-transform', 'transform', 'hover:scale-105', 'border-solid', 'border', 'border-gray-200');
+                avisoCard.classList.add('block', 'p-6', 'bg-gray-800', 'rounded-lg', 'shadow-lg', 'text-center', 'transition-transform', 'transform', 'hover:scale-105', 'border-solid', 'border', 'border-gray-200');
                 avisoCard.innerHTML = `
-                    <h2 class="text-xl font-semibold mb-4">${aviso.titulo_Aviso}</h2>
-                    <p class="text-gray-800 mb-4">${aviso.contenido_Aviso}</p>
-                    <p class="text-gray-800 mb-4">Publicado el: ${new Date(aviso.fecha_Publicacion).toLocaleDateString()}</p>
-                    <p class="text-gray-800 mb-4">Creador: ${aviso.nombre_Creador}</p>
+                    <h2 class="text-xl font-semibold mb-4 text-white">${aviso.titulo_Aviso}</h2>
+                    <p class="text-white mb-4">${aviso.contenido_Aviso}</p>
+                    <p class="text-white mb-4">Publicado el: ${new Date(aviso.fecha_Publicacion).toLocaleDateString()}</p>
+                    <p class="text-white mb-4">Creador: ${aviso.nombre_Creador}</p>
                 `;
                 avisosContainer.appendChild(avisoCard);
             });
--- maquetadoRuth/output/tramites.js
+++ maquetadoRuth/output/tramites.js
@@ -8,13 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
             tramites.forEach(tramite => {
                 const tramiteElement = document.createElement('div');
-                tramiteElement.classList.add('block', 'p-6', 'bg-white', 'rounded-lg', 'shadow-lg', 'text-center', 'transition-transform', 'transform', 'hover:scale-105', 'border-solid', 'border', 'border-gray-200');
+                tramiteElement.classList.add('block', 'p-6', 'bg-gray-800', 'rounded-lg', 'shadow-lg', 'text-center', 'transition-transform', 'transform', 'hover:scale-105', 'border-solid', 'border', 'border-gray-200');
                 tramiteElement.innerHTML = `
-                    <h3 class="text-xl font-semibold mb-4">${tramite.titulo_Tramite}</h3>
-                    <p class="text-gray-600 mb-4">${tramite.descripcion_Tramite}</p>
-                    <p class="text-gray-600 mb-4"><strong>Fecha de Publicación:</strong> ${tramite.fecha_Publicacion}</p>
-                    <p class="text-gray-600 mb-4"><strong>Fecha de Cierre:</strong> ${tramite.fecha_Cierre}</p>
-                    <p class="text-gray-600 mb-4"><strong>Nombre del Creador:</strong> ${tramite.nombre_Creador}</p>
+                    <h3 class="text-xl font-semibold mb-4 text-white">${tramite.titulo_Tramite}</h3>
+                    <p class="text-white mb-4">${tramite.descripcion_Tramite}</p>
+                    <p class="text-white mb-4"><strong>Fecha de Publicación:</strong> ${tramite.fecha_Publicacion}</p>
+                    <p class="text-white mb-4"><strong>Fecha de Cierre:</strong> ${tramite.fecha_Cierre}</p>
+                    <p class="text-white mb-4"><strong>Nombre del Creador:</strong> ${tramite.nombre_Creador}</p>
                     <a href="/download/payment/${tramite.id_Tramite}" class="text-blue-500 hover:underline">Descargar Ficha de Pago</a>
                 `;
                 tramitesContainer.appendChild(tramiteElement);
